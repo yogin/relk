6af5add3646850429db93dd6234f21f534901762
Anthony Powles <github@idreamz.net>
2015-04-06 14:39:07 -0700

    First attempt as configuring logstash

--- relk/attributes/default.rb
+++ relk/attributes/default.rb
@@ -5,3 +5,13 @@ default['relk']['htpasswd']['password'] = 'you_should_really_change_this'
 # nginx configuration
 default['relk']['nginx_kibana']['server_port'] = 80
 default['relk']['nginx_kibana']['server_name'] = 'localhost'
+
+# logstash input
+default['relk']['logstash']['input']['sqs']['access_key'] = ''
+default['relk']['logstash']['input']['sqs']['secret_key'] = ''
+default['relk']['logstash']['input']['sqs']['region'] = ''
+default['relk']['logstash']['input']['sqs']['queue'] = ''
+
+# logstash output
+default['relk']['logstash']['output']['elasticsearch']['host'] = ''
+default['relk']['logstash']['output']['elasticsearch']['cluster'] = ''
