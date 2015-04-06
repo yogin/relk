f5c0e2563a70226ac09d5a8334c8bb2322f4fa84
Anthony Powles <github@idreamz.net>
2015-04-06 15:04:06 -0700

    Fix logstash config

--- relk/recipes/logstash_config.rb
+++ relk/recipes/logstash_config.rb
@@ -1,28 +1,22 @@
 include_recipe "simple-logstash"
 
-#template "#{node['logstash']['prefix_root']}/pat/to/logstash.conf" do
-  #source "logstash.conf.erb"
-  #owner node['logstash']['user']
-  #group node['logstash']['group']
-  #mode '0600'
-  #variables({
-    #sqs_queue: node['relk']['logstash']['input']['sqs']['queue'],
-    #aws_access_key: node['relk']['logstash']['input']['sqs']['access_key'],
-    #aws_secret_key: node['relk']['logstash']['input']['sqs']['secret_key'],
-    #aws_region: node['relk']['logstash']['input']['sqs']['region'], 
-    #elasticsearch_host: node['relk']['logstash']['output']['elasticsearch']['host'],
-    #elasticsearch_cluster: node['relk']['logstash']['output']['elasticsearch']['cluster']
-  #})
-#end
-
-logstash_config 'sqs_to_es' do
-  source "logstash.conf.erb"
+logstash_input 'sqs' do
+  service 'logstash'
   variables({
     sqs_queue: node['relk']['logstash']['input']['sqs']['queue'],
     aws_access_key: node['relk']['logstash']['input']['sqs']['access_key'],
     aws_secret_key: node['relk']['logstash']['input']['sqs']['secret_key'],
     aws_region: node['relk']['logstash']['input']['sqs']['region'], 
+  })
+end
+
+logstash_output 'es' do
+  service 'logstash'
+  variables({
     elasticsearch_host: node['relk']['logstash']['output']['elasticsearch']['host'],
     elasticsearch_cluster: node['relk']['logstash']['output']['elasticsearch']['cluster']
   })
 end
+
+logstash_service 'logstash'
+
